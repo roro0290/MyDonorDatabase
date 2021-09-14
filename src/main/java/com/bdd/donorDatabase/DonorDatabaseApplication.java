654b251108b3b724362e73703651042327417740
Rohini <rohini_subramanian@outlook.com>
2021-09-14 19:46:42 +0800

    Create and initialise project

---
+++ src/main/java/com/bdd/donorDatabase/DonorDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.bdd.donorDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonorDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonorDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdd/donorDatabase/DonorDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdd.donorDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonorDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
