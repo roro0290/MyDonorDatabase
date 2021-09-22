e5f77bc958d879db4e84b0b419c71675f6ac4d82
Rohini <rohini_subramanian@outlook.com>
2021-09-22 16:39:37 +0800

    Create CheckAddDonor Tests

--- src/test/java/com/bdd/donorDatabase/DonorDatabaseApplicationTests.java
+++ src/test/java/com/bdd/donorDatabase/DonorDatabaseApplicationTests.java
@@ -1,13 +1,42 @@
 package com.bdd.donorDatabase;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class DonorDatabaseApplicationTests {
+
+	DonorController donorController = new DonorController();
+	Donor d1;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@BeforeEach
+	void setup(){ d1 = new Donor("Rohini",1234);}
+
+	@AfterEach
+	void cleanUp(){ d1 = null;}
+
+	@Test
+	void checkAddDonorReturnsTrue(){
+		assertEquals("added",donorController.addDonor(d1));
+	}
+
+	@Test
+	void checkAddDonorReturnsFalseWhenDuplicateDonor(){
+		donorController.addDonor(d1);
+		assertEquals("existing",donorController.addDonor(d1));
+	}
+
+	@Test
+	void checkFindDonorUsingNameReturnsTrue(){
+		assertNotNull(donorController.findDonorUsingName("ro"));
+	}
 }
