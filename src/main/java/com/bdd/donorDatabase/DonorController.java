90310343c01cbfd132acabad7bc23d69c69b7a0c
Rohini <rohini_subramanian@outlook.com>
2021-09-22 16:38:14 +0800

    Create DonorController and DonorService

---
+++ src/main/java/com/bdd/donorDatabase/DonorController.java
@@ -0,0 +1,11 @@
+package com.bdd.donorDatabase;
+
+public class DonorController {
+    DonorService donorService = new DonorService();
+
+    public String addDonor(Donor d){
+        return donorService.addNewDonor(d);
+    }
+
+    public Donor findDonorUsingName(String s){ return donorService.findDonorUsingName(s);}
+}
